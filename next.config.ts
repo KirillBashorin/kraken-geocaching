24dae19e6ce26a677cb31c62a1fa959cebf6ed8d
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2024-10-30 03:27:56 +0400

    Config update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    distDir: 'build',
+    output: 'export',
+    trailingSlash: true,
+    images: { unoptimized: true },
+    webpack: config => {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: [
+                {
+                    loader: '@svgr/webpack',
+                    options: {
+                        icon: true,
+                    },
+                },
+            ],
+        });
+
+        config.externals.push('pino-pretty', 'lokijs', 'encoding');
+
+        return config;
+    },
 };
 
 export default nextConfig;
